6adb419f8408cf00b400f8c4ac91d802a3897ca7
David Barron <90076120+webgeekbear@users.noreply.github.com>
2021-10-02 08:02:56 -0700

    Query for form data and add to DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,25 @@
 var formEl = document.querySelector("#task-form");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 formEl.addEventListener("submit", createTaskHandler);
 
 function createTaskHandler(event) {
-    event.preventDefault();
-    
-    var taskItemEl = document.createElement("li");
-    taskItemEl.textContent = "Hello World";
-    taskItemEl.className = "task-item";
+    event.preventDefault(); // Prevent screen refresh
 
-    var tasksEl = document.querySelector("#tasks-to-do");
-    tasksEl.appendChild(taskItemEl);
+    let taskNameInput = document.querySelector("input[name='task-name']").value;
+    let taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // Create new list item
+    let listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+
+    // Create div to hold task info and add to list item
+    let taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    // Add list item to the to do list
+    tasksToDoEl.appendChild(listItemEl);
 }
